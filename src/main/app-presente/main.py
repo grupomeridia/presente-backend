5356fdf38b3f9208f5f67cdf8bbe3b2af7c8b828
Tokram <xtokram@gmail.com>
2023-06-06 18:55:44 -0300

    Bug Fix

--- src/main/app-presente/main.py
+++ src/main/app-presente/main.py
@@ -1,8 +1,14 @@
 from entity.Aluno import Aluno
-from repository.AlunoRepository import AlunoRepository
+from entity.Chamada import Chamada
+from entity.Configuracao import Configuracao
+from entity.CursoEnum import Curso
+from entity.Presenca import Presenca
+from entity.PresencaEnum import TipoPresenca
+from entity.Professor import Professor
+from entity.Turma import Turma
+
 from repository.MainRepository import MainRepository
 
-AlunoRepository.criarTabela(MainRepository.app, MainRepository.db)
-
-
+with MainRepository.app.app_context():
+    MainRepository.db.create_all()
 
