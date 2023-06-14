230d5013cc39a79065a6735cd81879e3b1066231
Tokram <xtokram@gmail.com>
2023-06-14 00:41:10 -0300

    Corrigido classe ORM

--- src/main/app-presente/entity/Professor.py
+++ src/main/app-presente/entity/Professor.py
@@ -1,12 +1,15 @@
 from repository.MainRepository import MainRepository
 
+from entity.Chamada import Chamada
+from entity.Presenca import Presenca
+
 class Professor(MainRepository.db.Model):
     #__tablename__ = 'professores'
     id = MainRepository.db.Column(MainRepository.db.Integer, primary_key=True)
     ativo = MainRepository.db.Column(MainRepository.db.Boolean, nullable=False)
     nome = MainRepository.db.Column(MainRepository.db.String, nullable=False)
-    presenca = MainRepository.db.relationship('presenca', backref='professor')
-    chamada = MainRepository.db.relationship('chamada', backref='professor')
+    presenca = MainRepository.db.relationship('Presenca', backref='professor')
+    chamada = MainRepository.db.relationship('Chamada', backref='professor')
 
     def verificaAlunos() -> bool:
         #IMPEDITIVO: Falta criar a classe controller
