c1a14e3e6b79d2e4de624504b6d2775b97010d41
Tokram <xtokram@gmail.com>
2023-06-14 00:44:22 -0300

    Importado entities

--- src/main/app-presente/entity/Turma.py
+++ src/main/app-presente/entity/Turma.py
@@ -1,16 +1,23 @@
 from repository.MainRepository import MainRepository
 
-class Turma(MainRepository.db.Model):
+from entity.Chamada import Chamada
+from entity.Presenca import Presenca
 
-    #__tablename__ ='turmas'
+class Turma(MainRepository.db.Model):
     
     id = MainRepository.db.Column(MainRepository.db.Integer, primary_key=True)
     ativo = MainRepository.db.Column(MainRepository.db.Boolean, nullable=False)
     nome = MainRepository.db.Column(MainRepository.db.String(50), nullable=False)
     ano = MainRepository.db.Column(MainRepository.db.Integer, nullable=False)
     semestre = MainRepository.db.Column(MainRepository.db.String(1), nullable=False)
-    chamada = MainRepository.db.relationship('chamada', backref='turma')
-    presenca = MainRepository.db.relationship('presenca', backref='turma')
+    chamada = MainRepository.db.relationship('Chamada', backref='Turma')
+    presenca = MainRepository.db.relationship('Presenca', backref='Turma')
+
+    def __init__(self, ativo, nome, ano, semestre):
+        self.ativo = ativo
+        self.nome = nome
+        self.ano = ano
+        self.semestre = semestre
 
   
 
