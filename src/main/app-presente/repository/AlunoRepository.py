ad2ff1a1ce5ebd1a0a2244e1e011654df55528b2
Kript0n007 <gabrielkuhnen38@gmail.com>
2023-06-18 20:46:02 -0300

    fiz a funçao q verifica a presença do aluno

--- src/main/app-presente/repository/AlunoRepository.py
+++ src/main/app-presente/repository/AlunoRepository.py
@@ -11,3 +11,9 @@ class AlunoRepository():
             "Turma": Aluno.query.get(id).turma_id,
             "Curso": Aluno.query.get(id).curso.value
         }
+    
+    def verificarPresencaAluno(ra, turma_id, data):
+        data_formatada = datetime.strptime(data, '%Y-%m-%d').date()
+        presenca = Presenca.query.filter_by(aluno_ra=ra, turma_id=turma_id, horario=data_formatada).first()
+        
+        return presenca is not None
