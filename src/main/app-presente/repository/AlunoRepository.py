620d2d72a4b2328bb48abb1c8244c56437b48b1d
roberto <rvpassos@hotmail.com>
2023-06-22 16:36:00 -0300

    requisicao get de listAll

--- src/main/app-presente/repository/AlunoRepository.py
+++ src/main/app-presente/repository/AlunoRepository.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from repository.MainRepository import MainRepository
 
 from entity.Aluno import Aluno
@@ -5,9 +6,23 @@ from entity.Aluno import Aluno
 class AlunoRepository():
     def getAlunoById(id):
         return {
+            "Id": Aluno.query.get(id).id,
             "Nome": Aluno.query.get(id).nome,
-            "ID": Aluno.query.get(id).ativo,
-            "Ativo": Aluno.query.get(id).ra,
+            "RA": Aluno.query.get(id).ra,
+            "Ativo": Aluno.query.get(id).ativo,
             "Turma": Aluno.query.get(id).turma_id,
             "Curso": Aluno.query.get(id).curso.value
         }
+    
+    def listAll():
+        alunos = Aluno.query.all()
+        resultado = [{
+            "Id": a.id,
+            "Nome": a.nome,
+            "RA": a.ra,
+            "Ativo": a.ativo,
+            "Turma": a.turma_id,
+            "Curso": a.curso.value
+        } for a in alunos]
+
+        return jsonify(resultado)
