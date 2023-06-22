68c1f7e03bebfae518d229bcf840e020f2bccbea
roberto <rvpassos@hotmail.com>
2023-06-22 16:32:30 -0300

    Resolvi erro de property

--- src/main/app-presente/entity/Presenca.py
+++ src/main/app-presente/entity/Presenca.py
@@ -18,13 +18,12 @@ class Presenca(MainRepository.db.Model):
     tipo_presenca = MainRepository.db.Column(MainRepository.db.Enum(TipoPresenca))
     horario = MainRepository.db.Column(MainRepository.db.DateTime(timezone=True))
 
-    def __init__(self, id:int, ativo:bool, aluno_ra:int, turma_id:int, projeto_id:int, chamada_id:int, professor_id:int, tipo_presenca:TipoPresenca, horario:datetime):
-        self._id = id
-        self._ativo = ativo
-        self._aluno_ra = aluno_ra
-        self._turma_id = turma_id
-        self._projeto_id = projeto_id
-        self._chamada_id = chamada_id
-        self._professor_id = professor_id
-        self._tipo_presenca = tipo_presenca
-        self._horario = horario
+    def __init__(self, ativo:bool, aluno_ra:int, turma:int, projeto:int, chamada:int, professor:int, tipo_presenca:TipoPresenca, horario:datetime):
+        self.ativo = ativo
+        self.aluno_ra = aluno_ra
+        self.turma_id = turma
+        self.projeto_id = projeto
+        self.chamada_id = chamada
+        self.professor_id = professor
+        self.tipo_presenca = tipo_presenca
+        self.horario = horario
