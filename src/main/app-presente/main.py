5495509ed31ffbe6908452f102a9a48aefcec40f
roberto <rvpassos@hotmail.com>
2023-06-22 16:34:12 -0300

    Rotas da maioria das classes adicionadas

--- src/main/app-presente/main.py
+++ src/main/app-presente/main.py
@@ -11,13 +11,17 @@ from entity.Projeto import Projeto
 from repository.MainRepository import MainRepository
 from repository.TurmaRepository import TurmaRepository
 from repository.AlunoRepository import AlunoRepository
+from repository.ProfessorRepository import ProfessorRepository
+from repository.ProjetoRepository import ProjetoRepository
+from repository.ChamadaRepository import ChamadaRepository
+from repository.PresencaRepository import PresencaRepository
 from flask import Flask, request, jsonify
 
 with MainRepository.app.app_context():
     MainRepository.db.create_all()
 
 
-@MainRepository.app.route("/api/cadastrarAluno", methods=["POST"])
+@MainRepository.app.route("/api/aluno/cadastrar", methods=["POST"])
 def cadastrarAluno():
     data = request.json
 
@@ -32,14 +36,17 @@ def cadastrarAluno():
 
     return "Aluno Cadastrado!"
 
-
-@MainRepository.app.route("/api/listarAluno", methods=['GET'])
+@MainRepository.app.route("/api/aluno/findById", methods=['GET'])
 def listarAlunos():
     id = request.args.get('id')
     
     return jsonify(AlunoRepository.getAlunoById(id))
 
-@MainRepository.app.route("/api/cadastrarTurma", methods=['POST'])
+@MainRepository.app.route("/api/aluno/listAll", methods=['GET'])
+def listAllAlunos():
+    return AlunoRepository.listAll()
+
+@MainRepository.app.route("/api/turma/cadastrar", methods=['POST'])
 def cadastrarTurma():
     data = request.json
 
@@ -53,12 +60,115 @@ def cadastrarTurma():
 
     return "Turma Cadastrada!"
 
-@MainRepository.app.route("/api/listarTurma", methods=['GET'])
+@MainRepository.app.route("/api/turma/findById", methods=['GET'])
 def listarTurmas():
     id = request.args.get('id')
     print(TurmaRepository.getTurmaById(id))
     return jsonify(TurmaRepository.getTurmaById(id))
     
-    
+#Rotas do Professor
+@MainRepository.app.route("/api/professor/cadastrar", methods=['POST'])
+def cadastrarProfessor():
+    data = request.json
+
+    ativo = data['ativo']
+    nome = data['nome']
+
+    MainRepository.db.session.add(Professor(ativo, nome))
+    MainRepository.db.session.commit()
+
+    return "Professor Cadastrado!"
+
+@MainRepository.app.route("/api/professor/findById", methods=['GET'])
+def listarProfessor():
+    id = request.args.get('id')
+
+    return jsonify(ProfessorRepository.getProfessorById(id)) 
+
+
+@MainRepository.app.route("/api/professor/listAll", methods=['GET'])
+def listarAllProfessores():
+    return ProfessorRepository.listAll()
+
+#Rotas de Projeto
+@MainRepository.app.route("/api/projeto/cadastrar", methods=['POST'])
+def cadastrarProjeto():
+    data = request.json
+
+    ativo = data['ativo']
+    nome = data['nome']
+
+    MainRepository.db.session.add(Projeto(ativo, nome))
+    MainRepository.db.session.commit()
+
+    return "Projeto Cadastrado!"
+
+@MainRepository.app.route("/api/projeto/findById", methods=['GET'])
+def listarProjeto():
+    id = request.args.get('id')
+
+    return jsonify(ProjetoRepository.getProjetoById(id))
+
+@MainRepository.app.route("/api/projeto/listAll", methods=['GET'])
+def listarAllProjetos():
+    return ProjetoRepository.listAll()
+
+#Rotas de Chamada
+@MainRepository.app.route("/api/chamada/cadastrar", methods=['POST'])
+def cadastrarChamada():
+    data = request.json
+
+    ativo = data['ativo']
+    projeto = data['projeto']
+    professor = data['professor']
+    turma = data['turma']
+
+    MainRepository.db.session.add(Chamada(ativo, projeto, professor, turma))
+    MainRepository.db.session.commit()
+
+    return "Chamada Cadastrada!"
+
+@MainRepository.app.route("/api/chamada/findById", methods=['GET'])
+def listarChamada():
+    id = request.args.get('id')
+
+    return jsonify(ChamadaRepository.getChamadaById(id))
+
+@MainRepository.app.route("/api/chamada/listAll", methods=['GET'])
+def listarAllChamadas():
+    return ChamadaRepository.listAll()
+
+
+@MainRepository.app.route("/api/presenca/cadastrar", methods=['POST'])
+def cadastrarPresente():
+    data = request.json
+
+    ativo = data['ativo']
+    aluno_ra = data['aluno_ra']
+    turma = data['turma']
+    projeto = data['projeto']
+    chamada = data['chamada']
+    professor = data['professor']
+    tipo_presenca = data['tipo_presenca']
+    horario = data['horario']
+
+    MainRepository.db.session.add(Presenca(ativo, aluno_ra, turma, projeto, chamada, professor, tipo_presenca, horario))
+    MainRepository.db.session.commit()
+
+    return "Presenca Cadastrada!"
+
+@MainRepository.app.route("/api/presenca/findById", methods=['GET'])
+def listarPresenca():
+    id = request.args.get('id')
+
+    return jsonify(PresencaRepository.getAlunoById(id))
+
+@MainRepository.app.route("/api/presenca/listAll", methods=['GET'])
+def listAllPresencas():
+    return PresencaRepository.listAll()
+
+@MainRepository.app.route("/api/presenca/findByPresentes", methods=['GET'])
+def findByPresentes():
+    return PresencaRepository.findByPresentes()
 
 MainRepository.app.run(debug=True)
