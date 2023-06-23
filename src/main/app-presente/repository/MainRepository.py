a7bb01409b1f6eddf8af9ee196f362f5f476b047
tokram <xtokram@gmail.com>
2023-06-23 17:50:30 -0300

    Refatoração estrutura de pastas

--- src/main/app-presente/repository/MainRepository.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-class MainRepository():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/presente'
-    db = SQLAlchemy(app)
