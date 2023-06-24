4cce70b6230e5fb87cb20679650f00e3e8d06e3d
roberto <rvpassos@hotmail.com>
2023-06-24 18:38:22 -0300

    PUT, DELETE Methods e Reestruturacao da route

--- src/blueprints/ChamadaBlueprint.py
+++ src/blueprints/ChamadaBlueprint.py
@@ -7,25 +7,33 @@ from entity.Chamada import Chamada
 
 chamadas = Blueprint("chamadas", __name__)
 
-@chamadas.route("/api/chamada/cadastrar", methods=['POST'])
-def cadastrarChamada():
-    data = request.json
+@chamadas.route("/api/chamada", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def professor():
+    if request.method == 'GET':
+        id = request.args.get('id')
+        return jsonify(ChamadaRepository.getChamadaById(id))
+    
+    if request.method == 'POST':
+        data = request.json
 
-    ativo = data['ativo']
-    projeto = data['projeto']
-    professor = data['professor']
-    turma = data['turma']
+        ativo = data['ativo']
+        projeto = data['projeto']
+        professor = data['professor']
+        turma = data['turma']
 
-    MainRepository.db.session.add(Chamada(ativo, projeto, professor, turma))
-    MainRepository.db.session.commit()
+        MainRepository.db.session.add(Chamada(ativo, projeto, professor, turma))
+        MainRepository.db.session.commit()
 
-    return "Chamada Cadastrada!"
+        return "Chamada Cadastrada!"
 
-@chamadas.route("/api/chamada/findById", methods=['GET'])
-def listarChamada():
-    id = request.args.get('id')
-
-    return jsonify(ChamadaRepository.getChamadaById(id))
+    if request.method == 'PUT':
+        id = request.args.get('id')
+        data = request.json
+        return jsonify(ChamadaRepository.update(id, data))
+    
+    if request.method == 'DELETE':
+        id = request.args.get('id')
+        return jsonify(ChamadaRepository.delete(id))
 
 @chamadas.route("/api/chamada/listAll", methods=['GET'])
 def listarAllChamadas():
