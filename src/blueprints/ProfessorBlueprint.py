4316a3c0ebeb2ef1564976777158c161c5069339
roberto <rvpassos@hotmail.com>
2023-06-24 17:57:05 -0300

    PUT, DELETE Methods e Reestruturacao da route

--- src/blueprints/ProfessorBlueprint.py
+++ src/blueprints/ProfessorBlueprint.py
@@ -7,25 +7,42 @@ from entity.Professor import Professor
 
 professores = Blueprint("professores", __name__)
 
-@professores.route("/api/professor/cadastrar", methods=['POST'])
-def cadastrarProfessor():
-    data = request.json
+@professores.route("/api/professor", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def professor():
+   if request.method == 'GET':
+       id = request.args.get('id')
 
-    ativo = data['ativo']
-    nome = data['nome']
 
-    MainRepository.db.session.add(Professor(ativo, nome))
-    MainRepository.db.session.commit()
+       return jsonify(ProfessorRepository.getProfessorById(id))
 
-    return "Professor Cadastrado!"
 
-@professores.route("/api/professor/findById", methods=['GET'])
-def listarProfessor():
-    id = request.args.get('id')
+   if request.method == 'POST':
+       data = request.json
 
-    return jsonify(ProfessorRepository.getProfessorById(id)) 
+
+       ativo = data['ativo']
+       nome = data['nome']
+
+
+       MainRepository.db.session.add(Professor(ativo, nome))
+       MainRepository.db.session.commit()
+
+
+       return "Professor Cadastrado!"
+
+
+   if request.method == 'PUT':
+       id = request.args.get('id')
+       data = request.json
+       return jsonify(ProfessorRepository.update(id, data))
+  
+   if request.method == 'DELETE':
+       id = request.args.get("id")
+       return jsonify(ProfessorRepository.delete(id))
+
+
 
 
 @professores.route("/api/professor/listAll", methods=['GET'])
 def listarAllProfessores():
-    return ProfessorRepository.listAll()
+   return ProfessorRepository.listAll()
