03c6248b0a01a944b60c20f4896ac83714605594
Tokram <xtokram@gmail.com>
2023-06-25 15:12:13 -0300

    Bug Fix

--- src/entity/Configuracao.py
+++ src/entity/Configuracao.py
@@ -6,7 +6,7 @@ class Configuracao(MainRepository.db.Model):
     turma_id = MainRepository.db.Column(MainRepository.db.Integer, MainRepository.db.ForeignKey('turma.id'))
     projeto_id = MainRepository.db.Column(MainRepository.db.Integer, MainRepository.db.ForeignKey('projeto.id'))
 
-    def __init__(self, ativo:bool, turma=int, projeto=int):
+    def __init__(self, ativo:bool, turma:int, projeto:int):
         self.ativo = ativo
         self.turma_id = turma
         self.projeto_id = projeto
