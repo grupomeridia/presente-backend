14694b8fbded11da79ab401fdc42bd2d28de1290
Kript0n007 <gabrielkuhnen38@gmail.com>
2023-07-03 04:05:07 -0300

    update

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,12 +1,43 @@
-import styles from "./Home.module.css";
-import NavBar from "../../componets/layout/Navbar";
-import Footer from "../../componets/layout/Footer";
 import React, { useState, useEffect } from "react";
 import api from "../../client/api";
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+import NavBar from "../../componets/layout/Navbar";
+import Footer from "../../componets/layout/Footer";
 
 function Home() {
   const [alunos, setAlunos] = useState([]);
+  const [ativo, setAtivo] = useState(false);
+  const [alunoRa, setAlunoRa] = useState("");
+  const [turma, setTurma] = useState("");
+  const [projeto, setProjeto] = useState("");
+  const [chamada, setChamada] = useState("");
+  const [professor, setProfessor] = useState("");
+  const [tipoPresenca, setTipoPresenca] = useState("");
+  const [horario, setHorario] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const presencaData = {
+      ativo: true,
+      AlunoRa: alunoRa,
+      turma,
+      projeto,
+      chamada,
+      professor,
+      tipoPresenca,
+      horario,
+    };
+
+    try {
+      const response = await api.post("/presenca", presencaData);
+      console.log(response.data);
+      // Tratar a resposta ou redirecionar para outra página
+    } catch (error) {
+      console.error("Erro ao marcar presença", error);
+    }
+  };
 
   useEffect(() => {
     const fetchAlunos = async () => {
@@ -21,50 +52,43 @@ function Home() {
     fetchAlunos();
   }, []);
 
-
-
   return (
     <div>
       <NavBar />
-      <body onload="tabela(lista)">
-        <section class={styles.page_content}>
-          <section class={styles.search_and_user}>
-            <div class={styles.admin_profile}>
-              <span class={styles.greeting}>Hello admin</span>
-              <i class="fa-solid fa-circle-user"></i>
+      <body onLoad="tabela(lista)">
+        <section className={styles.page_content}>
+          <section className={styles.search_and_user}>
+            <div className={styles.admin_profile}>
+              <span className={styles.greeting}>Hello admin</span>
+              <i className="fa-solid fa-circle-user"></i>
             </div>
           </section>
-          <section class={styles.grid}>
-            <table id="example">
-              <thead>
-                <tr>
-                  <th>Aluno</th>
-                  <th>Ra</th>
-                  <th>Cursos</th>
-                  <th>Turma</th>
-                  <th>Ações</th>
-                </tr>
-              </thead>
-              <tbody id="tbody">
-                {alunos.map((aluno) => (
-                  <tr key={aluno.Id}>
-                    <td>{aluno.Nome}</td>
-                    <td>{aluno.RA}</td>
-                    <td>{aluno.Curso}</td>
-                    <td>{aluno.Turma}</td>
-                    <td>
-                      <Link to = '/editar-aluno/' ><i class="fa-solid fa-user-pen"></i></Link>
-                      <i class="fa-solid fa-user-xmark"></i>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <section className={styles.grid}>
+            <div>
+              <h1>Marcar Presença</h1>
+              <form onSubmit={handleSubmit} className={styles.form}>
+                <label htmlFor="alunoRa" className={styles.label}>
+                  RA do Aluno:
+                </label>
+                <input
+                  type="text"
+                  name="alunoRa"
+                  id="alunoRa"
+                  value={alunoRa}
+                  onChange={(e) => setAlunoRa(e.target.value)}
+                  className={styles.input}
+                />
+                <br />
+                <button type="submit" className={styles.button}>
+                  Marcar Presença
+                </button>
+              </form>
+            </div>
           </section>
         </section>
         <script
           src="https://kit.fontawesome.com/a146d88807.js"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
         <script src="//code.jquery.com/jquery-3.3.1.js"></script>
         <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
