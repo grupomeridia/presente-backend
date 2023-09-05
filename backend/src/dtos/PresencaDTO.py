0c977a1820e55209f5ec1616ab453d524eb13744
LuizHenz <robertgito@hotmail.com>
2023-09-04 21:22:18 -0300

    PresencaDTO

--- backend/src/dtos/PresencaDTO.py
+++ backend/src/dtos/PresencaDTO.py
@@ -1,5 +1,7 @@
+from PresencaEnum import TipoPresenca
+import datetime
 class PresencaDTO:
-    def __init__(self, status, tipoPresenca, horario):
+    def __init__(self, status:bool, tipoPresenca:TipoPresenca, horario:datetime):
         self.status = status
         self.tipoPresenca = tipoPresenca
         self.horario = horario
