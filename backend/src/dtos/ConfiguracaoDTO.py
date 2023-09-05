b2d203e09b321366e9fc8627202fb5f955414916
LuizHenz <robertgito@hotmail.com>
2023-09-04 21:21:05 -0300

    ConfiguracaoDTO

--- backend/src/dtos/ConfiguracaoDTO.py
+++ backend/src/dtos/ConfiguracaoDTO.py
@@ -1,5 +1,6 @@
+import datetime
 class ConfiguracaoDTO:
-    def __init__(self, status, alunoAusente, inicioAula, finalAula):
+    def __init__(self, status:bool, alunoAusente:bool, inicioAula:datetime, finalAula:datetime):
         self.status = status
         self.alunoAusente = alunoAusente
         self.inicioAula = inicioAula
