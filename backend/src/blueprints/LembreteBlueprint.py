2a871092aae2fe076f3ffdc3ef7246173f9ae264
LuizHenz <robertgito@hotmail.com>
2023-09-11 12:04:01 -0300

    CRUD de lembretes

---
+++ backend/src/blueprints/LembreteBlueprint.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, request, jsonify
+
+from repository.LembreteRepository import LembreteRepository
+
+from service.LembreteService import LembreteService
+
+lembretes = Blueprint("lembretes", __name__)
+
+@lembretes.route("/api/lembrete", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def lembrete():
+    if request.method == 'GET':
+        id = request.args.get('id')
+        try:
+            return jsonify(LembreteService.getById(id))
+        except AssertionError as error:
+            return str(error)
+    
+    if request.method == 'POST':
+        data = request.json
+        
+        destinatarioCargo = data['destinatario']
+        titulo = data['titulo']
+        mensagem = data['mensagem']
+        criacao = data['criacao']
+        visualizacao = data['visualizacao']
+        
+        try:
+            return LembreteService.register(destinatarioCargo, titulo, mensagem, criacao, visualizacao)
+        except AssertionError as error:
+            return str(error)
+    
+    if request.method == 'PUT':
+        id = request.args.get('id')
+        data = request.json
+        
+        destinatarioCargo = data['destinatario']
+        titulo = data['titulo']
+        mensagem = data['mensagem']
+        criacao = data['criacao']
+        visualizacao = data['visualizacao']
+        
+        try:
+            return jsonify(LembreteService.update(destinatarioCargo, titulo, mensagem, criacao, visualizacao))
+        except AssertionError as error:
+            return str(error)
+    
+    if request.method == 'DELETE':
+        id = request.args.get('id')
+        try:
+            return jsonify(LembreteService.delete(id))
+        except AssertionError as error:
+            return str(error)
+    
+@lembretes.route("/api/lembrete/listAll", methods=['GET'])
+def listAll():
+    return LembreteRepository.listaAll()
