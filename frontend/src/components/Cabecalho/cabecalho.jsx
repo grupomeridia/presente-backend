1176a4f3d8def317723dbcdf7c88a59d9e847c02
Kript0n007 <gabrielkuhnen38@gmail.com>
2023-09-17 18:34:17 -0300

    maria q comitou (gabriel fez o cabeçalho)

--- frontend/src/components/Cabecalho/cabecalho.jsx
+++ frontend/src/components/Cabecalho/cabecalho.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
-import style from './cabecalho.module.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+import style from "./cabecalho.module.css";
 
 const Cabecalho = () => {
   return (
-    <div className={style.cabecalho}>
-      <span className={style.headerTitle}>App Chamada</span>
-      <FontAwesomeIcon icon={faCog} className={style.headerIcon} />
+    <div className={style.cabecalho_center}>
+      <div className={style.cabecalho}>
+        <span className={style.headerTitle}>App Chamada</span>
+        <FontAwesomeIcon icon={faCog} className={style.headerIcon} />
+      </div>
     </div>
   );
 };
