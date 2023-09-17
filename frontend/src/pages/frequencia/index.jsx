1e8833b0877a2255b86364486cba078c3c41e840
Kript0n007 <gabrielkuhnen38@gmail.com>
2023-09-17 18:44:03 -0300

    Tela Presença feita

--- frontend/src/pages/frequencia/index.jsx
+++ frontend/src/pages/frequencia/index.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import React, { useState } from 'react';
+import { Doughnut,Bar  } from 'react-chartjs-2';
 import NavBar from '@/components/Navbar/navbar';
 import { Fundo } from '@/components/Fundo/fundo';
+import styles from './style.module.css';
 import Chart from 'chart.js/auto';
-import styles from './style.module.css'
+import Cabecalho from '@/components/Cabecalho/cabecalho';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-const Frequencia = () => {
+Chart.register(ChartDataLabels);
 
+export default function Frequencia() {
   const [attendanceData, setAttendanceData] = useState(null);
   const [chartData, setChartData] = useState({
     labels: ['Red', 'Green', 'Blue'],
@@ -18,22 +21,55 @@ const Frequencia = () => {
           'rgba(255, 99, 132, 0.2)',
           'rgba(255, 159, 64, 0.2)',
           'rgba(255, 205, 86, 0.2)'
-        ]
+        ],
       }
     ]
   });
-  
+
+  const barChartData = {
+    labels: ['Red', 'Green', 'Blue'],
+    datasets: [
+      {
+        label: 'My Second Dataset',
+        data: [200, 100, 50],
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      }
+    ]
+  };
+
+  const barChartOptions = {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
 
   const chartOptions = {
     responsive: true,
-     maintainAspectRatio: false, 
+    maintainAspectRatio: false,
     plugins: {
+      datalabels: {
+        formatter: (value, context) => {
+          let sum = 0;
+          let dataArr = context.chart.data.datasets[0].data;
+          dataArr.map((data) => {
+            sum += data;
+          });
+          let percentage = (value * 100 / sum).toFixed(2) + "%";
+          return percentage;
+        },
+        color: '#fff',
+        anchor: 'center'
+      },
       legend: {
-        position: 'top',
+        position: 'left',
       },
       title: {
         display: true,
-        text: 'Chart.js Doughnut Chart',
+        text: '',
       },
     },
   };
@@ -42,22 +78,31 @@ const Frequencia = () => {
     return <div>Loading...</div>;
   }
 
-  const randomizeData = (chart) => {
+ return (
+  <>
+    <NavBar />
+    <Cabecalho/>
+      <Fundo className={styles.Fundo}>
+        <div className={styles.container_center}>
+          <div className={styles.tituloGrafico}>
+            <h2>Alunos Presentes</h2>
+          <div className={styles.graficoCircular}>
+            <Doughnut data={chartData} options={chartOptions} className={styles.Doughnut} />
+          </div>
+          </div>
+        </div>
+      </Fundo>
+      <Fundo className={styles.Fundo}>
+        <div className={styles.container_center}>
+          <div className={styles.tituloGrafico}>
+            <h2>Frequencia Semanal</h2>
+          <div className={styles.graficoBar}>
+          <Bar data={barChartData} options={barChartOptions} className={styles.Bar} />
+          </div>
+          </div>
+        </div>
+      </Fundo>
+  </>
+);
+}
 
-  };
-
-  return (
-    <>
-      <NavBar />
-      <h1>Cabeçalho</h1>
-      <button onClick={() => randomizeData()}>Randomize</button>
-      <div className={styles.container}>
-        <Fundo>
-          <Doughnut data={chartData} options={chartOptions} />
-        </Fundo>
-      </div>
-    </>
-  );
-};
-
-export default Frequencia;
