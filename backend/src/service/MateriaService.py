3676968aa96c26f993c4e00ffc4b555f12275554
LuizHenz <robertgito@hotmail.com>
2023-09-17 15:23:16 -0300

    Reorganização services

--- backend/src/service/MateriaService.py
+++ backend/src/service/MateriaService.py
@@ -4,7 +4,7 @@ from entity.Materia import Materia
 
 
 class MateriaService():
-    def getMateria(id):
+    def getById(id):
         try:
             int(id)
         except ValueError:
@@ -14,7 +14,29 @@ class MateriaService():
         assert Materia.query.get(id) != None, f"Nenhuma materia com o ID {id} foi encontrado"
         return MateriaRepository.getMateriaById(id)
     
-    def postMateria(ativo, nome):
+    def register(ativo, nome):
+
         assert ativo != None and ativo == True or False, "O campo ativo deve ser true ou false"
         assert len(str(nome)) > 5 and nome != None, "Nome inválido"
+
         return MateriaRepository.register(Materia(ativo, nome))
+    
+    def update(id, ativo, nome):
+        try:
+            int(id)
+        except ValueError:
+            raise AssertionError("ID deve ser um número inteiro")
+        assert int(id) > 0, "ID inválido."
+
+        return MateriaRepository.update(id, Materia(ativo, nome))
+    
+    def delete(id):
+        try:
+            int(id)
+        except ValueError:
+            raise AssertionError("ID deve ser um número inteiro.")
+        
+        assert int(id) > 0, "ID inválido"
+        assert Materia.query.filter(Materia.id == id).first() is not None, "Matéria não encontrada."
+        return MateriaRepository.delete(id)
+    
--- backend/src/service/ProfessorService.py
+++ backend/src/service/ProfessorService.py
@@ -3,18 +3,40 @@ from repository.ProfessorRepository import ProfessorRepository
 from entity.Professor import Professor
 
 class ProfessorService():
-    def getProfessor(id):
+    def getById(id):
+        try:
+            int(id)
+        except ValueError:
+            raise AssertionError("Deve ser um número inteiro.")
+        
+        assert int(id) > 0, "ID inválido."
+        assert Professor.query.get(id) != None, "Nenhum professor com este ID foi encontrado."
+
+        return ProfessorRepository.getProfessorById(id)
+    
+    def register(idProfessor, idUsuario, ativo, nome):
+        try:
+            int(idProfessor)
+            int(idUsuario)
+        except ValueError as error:
+            raise AssertionError("Campos obritório: Professor e usuário.")
+        
+        return ProfessorRepository.registerProfessor(Professor(idProfessor, idUsuario, ativo, nome))
+    
+    def update(id, idProfessor, idUsuario, ativo, nome):
+        try:
+            int(id)
+        except ValueError:
+            raise AssertionError("ID deve ser um número inteiro.")
+        assert int(id) > 0, "ID inválido."
+
+        return ProfessorRepository.update(id, Professor(idProfessor, idUsuario, ativo, nome))
+    
+    def delete(id):
         try:
             int(id)
         except ValueError:
             raise AssertionError("ID deve ser um número inteiro")
-        
-        assert int(id) > 0, "ID inválido!"
-        assert Professor.query.get(id) != None, f"Nenhum professor foi encontrado com o ID {id}"
-        return ProfessorRepository.getProfessorById(id)
-    
-    def postProfessor(ativo, nome):
-        assert ativo != None and ativo == True or False, "Campo ativo deve ser True ou False" 
-        assert len(str(nome)) > 3 and nome != None, "Nome inválido!"
-        
-        return ProfessorRepository.register(Professor(ativo, nome))
+        assert int(id) > 0, "ID inválido."
+        assert Professor.query.filter(Professor.id == id).first() is not None, "Professor não encontrado"
+        return ProfessorRepository.delete(id)
