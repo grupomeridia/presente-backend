2516226365a75aa37079733e4885b29c86d2f4dc
tokram <xtokram@gmail.com>
2023-09-18 10:22:21 -0300

    Validacao DTO

--- backend/src/dtos/PainelDTO.py
+++ backend/src/dtos/PainelDTO.py
@@ -1,7 +1,9 @@
 import datetime
+from pydantic import validate_arguments
 
 class PainelDTO:
-    def __init__(self, data:datetime, totalAtivos:int, totalPresentes:int, totalAusentes:int, totalPresentesCurso:list, totalAtivoCurso:list, totalAusenteCurso:list):
+    @validate_arguments
+    def __init__(self, data:datetime.date, totalAtivos:int, totalPresentes:int, totalAusentes:int, totalPresentesCurso:list, totalAtivoCurso:list, totalAusenteCurso:list):
         self.data = data
         self.totalAtivos = totalAtivos
         self.totalPresentes = totalPresentes
