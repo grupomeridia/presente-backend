53e9d40c10e6ac7cfea97e21db090a2f662430e8
roberto <rvpassos@hotmail.com>
2023-09-24 16:17:39 -0300

    fix entity

--- backend/src/entity/TurmaAluno.py
+++ backend/src/entity/TurmaAluno.py
@@ -1,6 +1,6 @@
 from repository.MainRepository import MainRepository
 
-turma_aluno = MainRepository.Table(
+turma_aluno = MainRepository.db.Table(
     'turma_aluno',
     MainRepository.db.Column('id_turma', MainRepository.db.Integer, MainRepository.db.ForeignKey('turmas.idTurma')),
     MainRepository.db.Column('id_aluno', MainRepository.db.Integer, MainRepository.db.ForeignKey('alunos.idALuno'))
