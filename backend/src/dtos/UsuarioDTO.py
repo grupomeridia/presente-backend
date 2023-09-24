d8a961cd61ba6c7460442157c075a9a17c20a1ee
roberto <rvpassos@hotmail.com>
2023-09-24 16:20:42 -0300

    validacao em usuarioDto

--- backend/src/dtos/UsuarioDTO.py
+++ backend/src/dtos/UsuarioDTO.py
@@ -1,6 +1,8 @@
-from CargoEnum import Cargo
+from entity.CargoEnum import Cargo
+from pydantic import validade_arguments
 
 class UsuarioDTO:
+    @validade_arguments
     def __init__(self, status:bool, login:str, senha:str, cargo:Cargo):
         self.status = status
         self.login = login
