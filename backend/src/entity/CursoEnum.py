d9becf18f43f357e80e695d4b611891e205360c6
roberto <rvpassos@hotmail.com>
2023-09-28 23:07:35 -0300

    bug fix

--- backend/src/entity/CursoEnum.py
+++ backend/src/entity/CursoEnum.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class Curso(Enum):
 
-    Engenharia_de_software = 'Engenharia de Software'
+    Engenharia_de_Software = 'Engenharia de Software'
     Analise_e_desenvolvimento_de_sistemas = 'Análise e Desenvolvimento de Sistemas'
 
 #Para acessar o valor do enum Curso.tipoEscolhido.value
