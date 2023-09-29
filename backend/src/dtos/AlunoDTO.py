a2f0415143927b775269e8add40875f638266373
roberto <rvpassos@hotmail.com>
2023-09-28 22:51:42 -0300

    bug fix

--- backend/src/dtos/AlunoDTO.py
+++ backend/src/dtos/AlunoDTO.py
@@ -3,7 +3,7 @@ from pydantic import validate_arguments
 class AlunoDTO:
     @validate_arguments
     def __init__(self, id_usuario:int, status:bool, ausente:bool, nome:str, ra:int):
-        self.id_usuario:id_usuario
+        self.id_usuario = id_usuario
         self.status = status
         self.ausente = ausente
         self.nome = nome
