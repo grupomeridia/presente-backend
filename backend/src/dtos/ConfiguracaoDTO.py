80f045db409f24995dbe2eee77d764597661abba
roberto <rvpassos@hotmail.com>
2023-09-29 15:21:59 -0300

    fix dtos

--- backend/src/dtos/ConfiguracaoDTO.py
+++ backend/src/dtos/ConfiguracaoDTO.py
@@ -3,8 +3,8 @@ from pydantic import validate_arguments
 
 class ConfiguracaoDTO:
     @validate_arguments
-    def __init__(self, status:bool, alunoAusente:bool, inicioAula:datetime, finalAula:datetime):
+    def __init__(self, status:bool, aluno_Ausente:bool, inicio_aula:datetime, final_aula:datetime):
         self.status = status
-        self.alunoAusente = alunoAusente
-        self.inicioAula = inicioAula
-        self.finalAula = finalAula
+        self.aluno_ausente = aluno_Ausente
+        self.inicio_aula = inicio_aula
+        self.final_aula = final_aula
