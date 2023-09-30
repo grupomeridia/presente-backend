46f68b194c3ac39589b11afd3dbf87250763ea3c
tokram <xtokram@gmail.com>
2023-09-30 14:42:51 -0300

    Refatoração Testes

---
+++ backend/src/settings_test.py
@@ -0,0 +1,13 @@
+TESTING = True
+DEBUG = False
+WTF_CSRF_ENABLED = False
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/app_presente'
+
+# password hash method
+PROJECT_PASSWORD_HASH_METHOD = 'md5'
+
+PROJECT_PASSWORD_HASH_METHOD = 'pbkdf2:sha1'
+PROJECT_SITE_NAME = u'Flask Example'
+PROJECT_SITE_URL = u'http://127.0.0.1:5000'
+PROJECT_SIGNUP_TOKEN_MAX_AGE = 60 * 60 * 24 * 7  # in seconds
+PROJECT_RECOVER_PASSWORD_TOKEN_MAX_AGE = 60 * 60 * 24 * 7  # in seconds
--- backend/src/tests/test_service/test_turma_service.py
+++
@@ -1,7 +0,0 @@
-from service.TurmaService import TurmaService
-
-class test_turma_service():
-    def test_deve_retornar_uma_turma():
-        
-        data = TurmaService.getTurma(id)
-        assert all(key in data for key in ["Ano", "Id", "Semestre", "curso", "modalidade", "nome", "status", "turno"])
