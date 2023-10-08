b615ba2292a1bf0deb7cecbb9c06de27e9383d9b
tokram <xtokram@gmail.com>
2023-10-08 02:08:41 -0300

    Novos padrões usuarios

--- backend/src/blueprints/UsuarioBlueprint.py
+++ backend/src/blueprints/UsuarioBlueprint.py
@@ -3,7 +3,6 @@ from flask import Blueprint, request, jsonify
 from repository.UsuarioRepository import UsuarioRepository
 
 from entity.Usuario import Usuario
-from dtos.UsuarioDTO import UsuarioDTO
 from entity.CargoEnum import Cargo
 
 from service.UsuarioService import UsuarioService
@@ -17,7 +16,7 @@ def usuario():
         try:
             return jsonify(UsuarioService.get_usuario_by_id(id_usuario))
         except AssertionError as error:
-            return str(error)
+            return str(error), 400
         
     if request.method == 'POST':
         data = request.json
@@ -27,28 +26,25 @@ def usuario():
         senha = data.get('senha', 'NOT_FOUND')
         cargo = data.get('cargo', 'NOT_FOUND')
 
-        if not cargo or cargo not in [cargo.value for cargo in Cargo]:
-            return jsonify({"error": "Campo 'cargo' inválido."}), 400
-
         try:
-            return UsuarioService.register(UsuarioDTO(status=status, login=login, senha=senha, cargo=cargo))
+            return UsuarioService.register(status, login, senha, cargo)
         except AssertionError as error:
-            return str(error)
+            return str(error), 400
         
     if request.method == 'PUT':
         id_usuario = request.args.get('id')
         data = request.json
 
         status = True
-        login = data['login']
-        senha = data['senha']
-        cargo = data['cargo']
+        login = data.get('login', 'NOT_FOUND')
+        senha = data.get('senha', 'NOT_FOUND')
+        cargo = data.get('cargo', 'NOT_FOUND')
 
-        return UsuarioService.update(id_usuario, UsuarioDTO(status=status, login=login, senha=senha, cargo=cargo))
-    
+        return UsuarioService.update(id_usuario, status, login, senha, cargo)
+
     if request.method == 'DELETE':
         id_usuario = request.args.get('id')
         try:
             return jsonify(UsuarioService.delete(id_usuario))
         except AssertionError as error:
-            return str(error)
+            return str(error), 400
