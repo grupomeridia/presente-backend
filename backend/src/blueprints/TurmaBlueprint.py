cfbfdcc49ce40361baf0b182b4b361e3960dce39
tokram <xtokram@gmail.com>
2023-10-08 02:08:29 -0300

    Retornando codigo de erro

--- backend/src/blueprints/TurmaBlueprint.py
+++ backend/src/blueprints/TurmaBlueprint.py
@@ -2,12 +2,6 @@ from flask import Blueprint, request, jsonify
 
 from repository.TurmaRepository import TurmaRepository
 
-from entity.Turma import Turma
-from dtos.TurmaDTO import TurmaDTO
-from entity.TurnoEnum import Turno
-from entity.ModalidadeEnum import Modalidade
-from entity.CursoEnum import Curso
-
 from service.TurmaService import TurmaService
 
 turmas = Blueprint("turmas", __name__)
@@ -19,9 +13,26 @@ def turma():
         try:
             return jsonify(TurmaService.get_turma(id_turma))
         except AssertionError as error:
-            return str(error)
+            return str(error), 400 
 
     if request.method == 'POST':
+        data = request.json
+        
+        status = True
+        nome = data.get('nome', 'NOT_FOUND')
+        ano = data.get('ano', 'NOT_FOUND')
+        semestre = data.get('semestre', 'NOT_FOUND')
+        turno = data.get('turno', 'NOT_FOUND')
+        modalidade = data.get('modalidade', 'NOT_FOUND')
+        curso = data.get('curso', 'NOT_FOUND')
+
+        try:
+            return TurmaService.post_turma(status, nome, ano, semestre, turno, modalidade, curso)
+        except AssertionError as error:
+            return str(error), 400
+
+    if request.method == 'PUT':
+        id_turma = request.args.get('id')
         data = request.json
 
         status = True
@@ -32,34 +43,7 @@ def turma():
         modalidade = data.get('modalidade', 'NOT_FOUND')
         curso = data.get('curso', 'NOT_FOUND')
 
-        if not turno or turno not in [turno.value for turno in Turno]:
-            return jsonify({"error":"Campo 'turno' inválido."}), 400
-        
-        if not modalidade or modalidade not in [modalidade.value for modalidade in Modalidade]:
-            return jsonify({"error":"Campo 'modalidade' inválido."}), 400
-        
-        if not curso or curso not in [curso.value for curso in Curso]:
-            return jsonify({"error":"Campo 'curso' inválido."}), 400
-
-        try:
-            return TurmaService.post_turma(TurmaDTO(status=status, nome=nome, ano=ano, semestre=semestre, turno=turno, modalidade=modalidade, curso=curso))
-        except AssertionError as error:
-            return str(error)
-
-    if request.method == 'PUT':
-        id_turma = request.args.get('id')
-        data = request.json
-
-        status = True
-        nome = data['nome']
-        ano = data['ano']
-        semestre = data['semestre']
-        turno = data['turno']
-        modalidade = data['modalidade']
-        curso = data['curso']
-
-
-        return TurmaService.update(id_turma, TurmaDTO(status=status, nome=nome, ano=ano, semestre=semestre, turno=turno, modalidade=modalidade, curso=curso))
+        return TurmaService.update(id_turma, status, nome, ano, semestre, turno, modalidade, curso)
     
     if request.method == 'DELETE':
         id_turma = request.args.get('id')
@@ -67,7 +51,7 @@ def turma():
         try:
             return jsonify(TurmaService.delete(id_turma))
         except AssertionError as error:
-            return str(error)
+            return str(error), 400
 
 @turmas.route("/api/turma/listAll", methods=['GET'])
 def listar_all_turmas():
