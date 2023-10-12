1f21ed7caa6d4db743a386f42edfc237dcae2528
roberto <rvpassos@hotmail.com>
2023-10-12 18:16:17 -0300

    refatoracao materia nas entidades

--- backend/src/entity/Materia.py
+++ backend/src/entity/Materia.py
@@ -5,7 +5,7 @@ class Materia(db.Model):
     id_materia = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.Boolean, nullable=False)
     nome = db.Column(db.String(100), nullable=False)
-    chamadas = db.relationship('Chamada', back_populates='materia')
+    chamadas = db.relationship('Turma', back_populates='materia')
 
     def __init__(self, status:bool, nome:str):
         self.status = status
