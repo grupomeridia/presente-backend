a651188de1cd231240a82ad5cf8b568ac5911c07
LuizHenz <robertgito@hotmail.com>
2023-10-12 18:16:01 -0300

    validacao service lembrete

--- backend/src/service/LembreteService.py
+++ backend/src/service/LembreteService.py
@@ -50,22 +50,52 @@ class LembreteService():
         return LembreteRepository.create(lembrete)
     
     @staticmethod
-    def update(id, lembrete_dto):
+    def update(id_lembrete, criacao, status, id_secretaria, destinatario_cargo, destinatario_id, titulo, mensagem):
 
-        lembrete = LembreteService.to_entity(id, lembrete_dto)
+        cargos = [x.value for x in Cargo]
+        assert destinatario_cargo in cargos, "Cargo inválido"
 
+        assert id != None, "Nenhum ID enviado."
+        assert int(id) if isinstance(id, (int,str)) and id.isdigit() else None, "ID incorreto."
+        assert int(id) > 0 and int(id) < 999999, "ID inválido."
+        assert Lembrete.query.get(id) != None, "Nenhum lembrete com este ID foi encontrado."
         
+        assert id_secretaria != 'NOT_FOUND', "Campo 'id_secretaria' inexistente."
+        assert destinatario_cargo != 'NOT_FOUND', "Campo 'destinatario_cargo' inexistente."
+        assert destinatario_id != 'NOT_FOUND', "Campo 'destinatario_id' inexistente."
+        assert titulo != 'NOT_FOUND', "Campo 'titulo' inexistente."
+        assert mensagem != 'NOT_FOUND', "Campo 'mensagem' inexistente."
+
+
+
+        assert int(id_secretaria) if isinstance(id_secretaria, (int,str)) and str(id_secretaria).isdigit() else None, "ID de secretaria incorreto."
+        assert int(id_secretaria) > 0 and int(id_secretaria) < 999999, "ID de secretaria inválido."
+        assert re.match(r'^\d+$', str(id_secretaria)), "O ID de secretaria deve ter apenas números."
+        secretaria = Secretaria.query.get(id_secretaria)
+        assert secretaria is not None, "Secretaria não encontrado"
+
+        assert int(destinatario_id) if isinstance(destinatario_id, (int,str)) and str(destinatario_id).isdigit() else None, "ID de destinatario incorreto."
+        assert int(destinatario_id) > 0 and int(destinatario_id) < 999999, "ID de destinatario inválido."
+        assert re.match(r'^\d+$', str(destinatario_id)), "O ID de destinatario deve ter apenas números."
+        
+        assert destinatario_id is not None, "Destinatario não encontrado"
+        assert LembreteRepository.get_cargo(destinatario_cargo, destinatario_id) == destinatario_id, "Destinatario incorreto."
+
+
+
+        lembrete = LembreteService.to_entity(LembreteDTO(criacao=criacao, status=status, id_secretaria=id_secretaria, destinatario_cargo=destinatario_cargo, destinatario_id=destinatario_id, titulo=titulo, mensagem=mensagem))
+
         return LembreteRepository.update(id, lembrete)
     
     @staticmethod
     def delete(id):
-        try:
-            int(id)
-        except ValueError:
-            raise AssertionError("ID deve ser um número inteiro.")
         
-        assert int(id) > 0, "ID inválido"
-        assert Lembrete.query.filter(Lembrete.id_lembrete == id).first() is not None, "Lembrete não encontrado."
+        assert id != None, "Nenhum ID enviado."
+        assert int(id) if isinstance(id, (int,str)) and id.isdigit() else None, "ID incorreto."
+        assert int(id) > 0 and int(id) < 999999, "ID inválido."
+        assert Lembrete.query.get(id) != None, "Nenhum lembrete com este ID foi encontrado."
+        
+
         return LembreteRepository.delete(id)
     
     @staticmethod
