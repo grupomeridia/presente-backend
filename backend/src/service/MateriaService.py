6a7b1a7a73592716fd3417d142ecfac60f70a458
LuizHenz <robertgito@hotmail.com>
2023-10-17 08:14:20 -0300

    update put service materia

--- backend/src/service/MateriaService.py
+++ backend/src/service/MateriaService.py
@@ -29,17 +29,20 @@ class MateriaService():
         return MateriaRepository.register(Materia(status=materia.status, nome=materia.nome))
     
     @staticmethod
-    def update(id_materia, nome):
+    def update(id_materia, nome, status):
 
         assert nome != 'NOT_FOUND', "Campo 'nome' inexistente."
-        assert re.match("^[a-zA-Z0-9]+$", nome), "O nome deve conter apenas letras e números."
+        assert re.match("^[a-zA-Z0-9 ]+$", nome), "O nome deve conter apenas letras e números."
         assert 3 <= len(nome) <= 50, "O nome deve ter entre 3 e 50 caracteres."
 
         assert not Materia.query.filter(Materia.nome == nome).first(), "Matéria ja cadastrada."
+        
+        materia = Materia.query.get(id_materia)
+        assert materia != None, "Matéria não encontrada."
 
-        materia = MateriaService.to_entity(MateriaDTO(id_materia=id_materia, nome=nome))
+        materia = MateriaService.to_entity(MateriaDTO(nome=nome, status=status))
 
-        return MateriaRepository.update(id, materia)
+        return MateriaRepository.update(id_materia, materia)
     
     @staticmethod
     def delete(id):
