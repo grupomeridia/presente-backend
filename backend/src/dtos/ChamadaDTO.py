35edffb719cd91ac2f815af9aa20c31f85c54a1e
LuizHenz <robertgito@hotmail.com>
2023-10-17 21:01:05 -0300

    update chamada dto

--- backend/src/dtos/ChamadaDTO.py
+++ backend/src/dtos/ChamadaDTO.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from pydantic import validate_arguments
-from typing import Optional
 
 class ChamadaDTO:
     @validate_arguments
-    def __init__(self, id_turma:int, id_professor:int, status:bool, abertura:datetime, encerramento: Optional[datetime] = None):
+    def __init__(self, id_turma:int, id_professor:int, status:bool, abertura:datetime, encerramento: datetime):
         self.id_turma = id_turma
         self.id_professor = id_professor
         self.status = status
