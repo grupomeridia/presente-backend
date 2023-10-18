ae702feb84c1b63cec4298ece87a726754eb3145
LuizHenz <robertgito@hotmail.com>
2023-10-17 21:00:25 -0300

    update blueprint chamada

--- backend/src/blueprints/ChamadaBlueprint.py
+++ backend/src/blueprints/ChamadaBlueprint.py
@@ -21,13 +21,13 @@ def professor():
         data = request.json
 
         status = True
-        abertura = datetime.now()
-        encerramento = None
+        abertura = data.get('abertura', 'NOT_FOUND')
+        encerramento = data.get('encerramento', 'NOT_FOUND')
         id_turma = data.get('id_turma', 'NOT_FOUND')
         id_professor = data.get('id_professor', 'NOT_FOUND')
         
         try:
-            return ChamadaService.register(id_turma=id_turma,id_professor=id_professor, status=status, abertura=abertura)
+            return ChamadaService.register(id_turma=id_turma,id_professor=id_professor, status=status, abertura=abertura, encerramento=encerramento)
         except AssertionError as error:
             return str(error), 400
 
