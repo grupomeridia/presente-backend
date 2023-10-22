861b342255e8a9a401a4a7dc4c4e1175943690e5
roberto <rvpassos@hotmail.com>
2023-10-22 14:30:30 -0300

    fix login e senha

--- backend/src/dtos/UsuarioDTO.py
+++ backend/src/dtos/UsuarioDTO.py
@@ -4,10 +4,10 @@ from typing import Optional
 
 class UsuarioDTO:
     @validate_arguments
-    def __init__(self, status:bool, username:str, password:str, nome:str, ra:Optional[int], cargo:Cargo):
+    def __init__(self, status:bool, login:str, senha:str, nome:str, ra:Optional[int], cargo:Cargo):
         self.status = status
-        self.username = username
-        self.password = password
+        self.login = login
+        self.senha = senha
         self.nome = nome
         self.ra = ra
         self.cargo = cargo
